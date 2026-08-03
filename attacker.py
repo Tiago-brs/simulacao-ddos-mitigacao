24bf07b99c1fb72a0aba19978b68412fc61615df
Thiago César Rodrigues <thiacefilho@gmail.com>
2026-08-03 12:23:19 -0300

    feat: implementar ataque POST flood

--- attacker.py
+++ attacker.py
@@ -17,25 +17,35 @@ URL_ALVO = f"http://{HOST_ALVO}:{PORTA_ALVO}{PATH_ALVO}"
 
 NUM_CLIENTES = 15               # quantidade de ips simulados
 REQUISICOES_POR_CLIENTE = 20    # quantidade de requisições por ip
+TIPO_ATAQUE = "post_flood"
+
+PAYLOAD_POST = {
+    "origem": "botnet_simulada",
+    "dados": "Olá!" * 2500  # envia 10000 caracteres
+}
 
 def gerar_ip_simulado(client_index: int) -> str:
     return f"127.0.0.{client_index + 2}"
 
+
+# seção ataque GET FLOOD
+
 # dispara apenas uma requisição HTTP simulando o ip de origem via o header X-Forwarded-For
 # e o cliente pega o header e le como o "IP do cliente", 
 # para simular diversos ips diferentes de forma fácil (TALVEZ USAR OUTRO MÉTODO PARA MULTIPLOS IPS)
-async def mandar_requisicao(cliente: httpx.AsyncClient, ip_origem: str) -> dict:
+async def mandar_requisicao_get(cliente: httpx.AsyncClient, ip_origem: str) -> dict:
     headers = {"X-Forwarded-For": ip_origem} if IS_WINDOWS else {}
 
     # requisição limpa, que o servidor descobre o ip pelo pacote TCP
     try:    
         response = await cliente.get(URL_ALVO, headers=headers)
         return {"ip": ip_origem, "status": response.status_code}
+
     except Exception as err:
         return {"ip": ip_origem, "status": "erro", "error": str(err)}
 
 # simula um cliente disparando vária requisições me sequencia rápida. amarrado ao seu próprio ip de origem
-async def simular_cliente(cliente_index: int) -> list:
+async def simular_cliente_get(cliente_index: int) -> list:
     ip_origem = gerar_ip_simulado(cliente_index)
     resultados_list =[]
 
@@ -43,26 +53,64 @@ async def simular_cliente(cliente_index: int) -> list:
         # modo windows: cria o cliente HTTP normal, sem forçar placa de rede
         async with httpx.AsyncClient() as cliente:
             for _ in range(REQUISICOES_POR_CLIENTE):
-                resultado = await mandar_requisicao(cliente, ip_origem)
+                resultado = await mandar_requisicao_get(cliente, ip_origem)
                 resultados_list.append(resultado)
     else:
         # modo linux/mac: "local_address" força a saída do tráfego pelo IP real específico
         transport = httpx.AsyncHTTPTransport(local_address=ip_origem)
         async with httpx.AsyncClient(transport=transport) as cliente:
             for _ in range(REQUISICOES_POR_CLIENTE):
-                resultado = await mandar_requisicao(cliente, ip_origem)
+                resultado = await mandar_requisicao_get(cliente, ip_origem)
                 resultados_list.append(resultado)
 
     return resultados_list
 
+
+# seção ataque POST FLOOD
+
+async def mandar_requisicao_post(cliente: httpx.AsyncClient, ip_origem: str) -> dict:
+    headers = {"X-Forwarded-For": ip_origem} if IS_WINDOWS else {}
+
+    try:    
+        # Envia a requisição POST acompanhada do payload JSON
+        response = await cliente.post(URL_ALVO, json=PAYLOAD_POST, headers=headers)
+        return {"ip": ip_origem, "status": response.status_code}
+
+    except Exception as err:
+        return {"ip": ip_origem, "status": "erro", "error": str(err)}
+
+async def simular_cliente_post(cliente_index: int) -> list:
+    ip_origem = gerar_ip_simulado(cliente_index)
+    resultados_list = []
+
+    if IS_WINDOWS:
+        async with httpx.AsyncClient() as cliente:
+            for _ in range(REQUISICOES_POR_CLIENTE):
+                resultado = await mandar_requisicao_post(cliente, ip_origem)
+                resultados_list.append(resultado)
+    else:
+        transport = httpx.AsyncHTTPTransport(local_address=ip_origem)
+        async with httpx.AsyncClient(transport=transport) as cliente:
+            for _ in range(REQUISICOES_POR_CLIENTE):
+                resultado = await mandar_requisicao_post(cliente, ip_origem)
+                resultados_list.append(resultado)
+
+    return resultados_list
+
+# execução do ataque
 async def rodar_ataque():
     print(f"Iniciando ataque simulado: {NUM_CLIENTES} clientes x {REQUISICOES_POR_CLIENTE} requisições cada")
     print(f"Alvo: {URL_ALVO}\n")
 
     hora_inicio = time.time()
 
-    # cria as tarefas, a função simular_cliente cria o cliente específico de cada IP
-    tasks = [simular_cliente(i) for i in range(NUM_CLIENTES)]
+    # cria as tarefas, a função simular_cliente_get cria o cliente específico de cada IP
+    # seleciona qual tipo de ataque vai simular
+    if TIPO_ATAQUE.lower() == "post_flood":
+        tasks = [simular_cliente_post(i) for i in range(NUM_CLIENTES)]
+    else:
+        tasks = [simular_cliente_get(i) for i in range(NUM_CLIENTES)]
+
     todos_resultados = await asyncio.gather(*tasks)
 
     # comprime a lista de listas em uma única lista
@@ -76,7 +124,7 @@ async def rodar_ataque():
         status = r["status"]
         sumario[status] = sumario.get(status, 0) + 1
 
-    print(f"Ataque concluído em {segundos_passado}s")
+    print(f"Ataque do tipo {TIPO_ATAQUE} concluído em {segundos_passado}s")
     print(f"Total de requisições enviadas: {len(flat_results)}")
     print("Resumo por status:", sumario)
     print("  200 = aceita | 429 = bloqueada pelo rate limiter | 403 = IP já banido")
